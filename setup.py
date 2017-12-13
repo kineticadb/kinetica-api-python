87854073e0de0c2d464130c54d9653c0f8b35518
Meem Mahmud <m_mahmud@bitbucket.com>
2017-12-13 15:42:12 -0500

    Cleanup

--- setup.py
+++ setup.py
@@ -17,12 +17,6 @@ def package_files(directory):
         if os.path.normpath(path) == directory :
             continue
 
-        # if (sys.version_info.major >= 3) and ("avro_py2" in path):
-        #     continue
-
-        # if (sys.version_info.major == 2) and ("avro_py3" in path):
-        #     continue
-
         for filename in filenames:
             file_basename, file_extension = os.path.splitext(filename)
             if file_extension and (file_extension.lower() != ".pyc"):
@@ -48,5 +42,4 @@ setup(
     package_data = {'gpudb': extra_files},
     url = 'http://www.kinetica.com',
     download_url = 'https://github.com/kineticadb/kinetica-api-python/archive/v6.1.0.1.tar.gz'
-    # download_url = 'https://github.com/kineticadb/kinetica-api-python'
 )
